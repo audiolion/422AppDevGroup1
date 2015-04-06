4c5508f4badd8d7128d2d8a3cefe70d23f0e0cf2
maxwellstorm <mxs5194@rit.edu>
2015-04-06 17:37:15 -0400

    finished edge conector test

--- AppDev/src/test/EdgeConnectorTest.java
+++ AppDev/src/test/EdgeConnectorTest.java
@@ -1,4 +1,5 @@
 package test;
+import main.EdgeConnector;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -6,79 +7,81 @@ import org.junit.Test;
 
 
 public class EdgeConnectorTest {
+        EdgeConnector testObj;
 
 	@Before
 	public void setUp() throws Exception {
+            testObj = new EdgeConnector("1|2|3|testStyle1|testStyle2");
 	}
 
-	@Test
-	public void testEdgeConnector() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testGetNumConnector() {
-		fail("Not yet implemented");
+		assertEquals("numConnector was intialized to 1 so it should be 1",1,testObj.getNumConnector());
 	}
 
 	@Test
 	public void testGetEndPoint1() {
-		fail("Not yet implemented");
+		assertEquals("EndPoint1 was intialized to 2",2,testObj.getEndPoint1());
 	}
 
 	@Test
 	public void testGetEndPoint2() {
-		fail("Not yet implemented");
+		assertEquals("EndPoint2 was intialzed as 3",3,testObj.getEndPoint2());
 	}
 
 	@Test
 	public void testGetEndStyle1() {
-		fail("Not yet implemented");
+		assertEquals("endStyle1 was intilized to \"testStyle1\"","testStyle1",testObj.getEndStyle1());
 	}
 
 	@Test
 	public void testGetEndStyle2() {
-		fail("Not yet implemented");
+		assertEquals("endStyle1 was intilized to \"testStyle1\"","testStyle2",testObj.getEndStyle2());
 	}
 
 	@Test
 	public void testGetIsEP1Field() {
-		fail("Not yet implemented");
+		assertEquals("isEP1Filed should be false",false,testObj.getIsEP1Field());
 	}
 
 	@Test
 	public void testGetIsEP2Field() {
-		fail("Not yet implemented");
+		assertEquals("IsEP2Fueld should be false",false,testObj.getIsEP2Field());
 	}
 
 	@Test
 	public void testGetIsEP1Table() {
-		fail("Not yet implemented");
+		assertEquals("isEP1Table should be false",false,testObj.getIsEP1Table());
 	}
 
 	@Test
 	public void testGetIsEP2Table() {
-		fail("Not yet implemented");
+		assertEquals("isEP2Table should be false",false,testObj.getIsEP2Table());
 	}
 
 	@Test
 	public void testSetIsEP1Field() {
-		fail("Not yet implemented");
+		testObj.setIsEP1Field(true);
+                assertEquals("isEP1Filed should be what you set it to",true,testObj.getIsEP1Field());
 	}
 
 	@Test
 	public void testSetIsEP2Field() {
-		fail("Not yet implemented");
+		testObj.setIsEP2Field(true);
+                assertEquals("isEP2Filed should be what you set it to",true,testObj.getIsEP2Field());
 	}
 
 	@Test
 	public void testSetIsEP1Table() {
-		fail("Not yet implemented");
+		testObj.setIsEP1Table(true);
+                assertEquals("isEp1Table should be what you set it to",true,testObj.getIsEP1Table());
 	}
 
 	@Test
 	public void testSetIsEP2Table() {
-		fail("Not yet implemented");
+		testObj.setIsEP2Table(true);
+                assertEquals("isEp2Table should be what you set it to",true,testObj.getIsEP2Table());
 	}
 
 }
